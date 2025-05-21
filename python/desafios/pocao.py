d9793171a03ef4285fa577fb97ec7be37293489d
JohnVSass <58093352+JohnVictorSA@users.noreply.github.com>
2025-05-21 23:44:58 +0000

    Pocao atualizada

--- python/desafios/pocao.py
+++ python/desafios/pocao.py
@@ -1,8 +1,45 @@
+
+
+""" FLUXO
+- Player está vivo?
+- usar poção (verde, roxa)
+- tratar casos (energia)
+- status
+- Personagem:
+    [Inventário] -> Item (arma, poção) # Associação/Agregação
+    [Armadura] # Composição
+"""
+
+class Item: 
+    def __init__(self, tipo:str, efeito:int):
+        self.tipo = tipo
+        self.efeito = efeito
+        
+class Inventario:
+    def __init__(self):
+        self.itens = [] #Agregação
+        
+    def adicionar_item(self, item: Item):
+        if not item:
+            print("Escolha um item para inserir")
+    
+        self.itens.append(item)
+            
+    def listar_itens(self):
+        for item in self.itens:
+            print(f"Itens {item.tipo}")
+            
+            
+# class Armadura:
+#       pass
+
+            
 class Personagem:
     def __init__(self, nome):
         self.nome = nome
         self.saude = 60
         self.vivo = True
+        self.inventario = Inventario() # Agregação 
         
     def usar_pocao(self, pocao):
         if (self.vivo == False):
@@ -38,13 +75,25 @@ class PocaoRoxa:
 p1 = Personagem("Pao")
 pocaoTop = PocaoVerde("Cura", 20)
 pocaoPaia = PocaoRoxa("Dano", 20)
+inventario = Inventario()
+faca = Item("Faca Tramontina", 120)
+escudo = Item("Escudo do rei", 90)
+
+inventario.adicionar_item(faca)
+inventario.adicionar_item(escudo)
+
+inventario.listar_itens()
+
+# print(p1.__dict__)
 
 
-p1.usar_pocao(pocaoTop)
-p1.usar_pocao(pocaoPaia)
-p1.usar_pocao(pocaoPaia)
-p1.usar_pocao(pocaoPaia)
-p1.usar_pocao(pocaoPaia)
-p1.usar_pocao(pocaoTop)
+
+
+# p1.usar_pocao(pocaoTop)
+# p1.usar_pocao(pocaoPaia)
+# p1.usar_pocao(pocaoPaia)
+# p1.usar_pocao(pocaoPaia)
+# p1.usar_pocao(pocaoPaia)
+# p1.usar_pocao(pocaoTop)
 
 
