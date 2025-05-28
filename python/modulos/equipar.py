184aee98a000b5a95359736e35179e63dc66b8ba
JohnVSass <58093352+JohnVictorSA@users.noreply.github.com>
2025-05-28 23:27:50 +0000

    Modulos aula dia 28/05

---
+++ python/modulos/equipar.py
@@ -0,0 +1,10 @@
+def dados():
+    print("Olá, sou o player")
+
+def equipar():
+    print("Equipando armadura")
+
+
+if __name__ == "__main__":
+    print("Eu sou o Player")
+# print(__name__)
---
+++ python/modulos/player.py
@@ -0,0 +1,12 @@
+def dados():
+    print("Olá, sou o player")
+
+def atacar():
+    print("Fight!!!")
+
+def equipar():
+    print("Equipando armadura")
+
+if __name__ == "__main__":
+    print("Eu sou o Player")
+# print(__name__)
